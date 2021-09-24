b8e363204550d7ab0348a5a54bf4381efde44168
Ryan Hung <ryan.ck.hung@gmail.com>
2021-09-24 22:18:58 +0800

    update on version

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
-require('chromedriver');
-const { Builder, By, Key, until } = require('selenium-webdriver');
+// the chromedriver should match with the chrome version
+// you shoud check with
+// https://chromedriver.chromium.org/downloads and update the package.json corespondingly
+
+require("chromedriver");
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
 (async function example() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  let driver = await new Builder().forBrowser("chrome").build();
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get("http://www.google.com/ncr");
+    await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+    await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
   } finally {
     // the following will close the browswer
     // await driver.quit();
